10d97cf6abdd4b5a85e680075c09b41afcc49d5f
Kendi Anyika <kendi.anyika@gmail.com>
2023-06-08 16:04:25 +0300

    undoing dependency injection

--- app/src/main/java/com/example/loginmvvm/ui/MyApplication.kt
+++ app/src/main/java/com/example/loginmvvm/ui/MyApplication.kt
@@ -1,7 +1,6 @@
 package com.example.loginmvvm.ui
 
 import android.app.Application
-import com.example.loginmvvm.di.AppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
@@ -9,20 +8,7 @@ import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 
-@HiltAndroidApp
-class MyApplication : Application()/*, HasAndroidInjector*/ {
-
-//    @Inject
-//    lateinit var mInjector: DispatchingAndroidInjector<Any>
-//    override fun onCreate(){
-//        super.onCreate()
-////        DaggerAppComponent.builder()
-//    }
-
-
-    //    override fun androidInjector(): AndroidInjector<Any> {
-//        return mInjector
-//    }
+class MyApplication : Application() {
 
 
 }
